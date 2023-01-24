b53180a07cf0fceba241fdee33db6e3d8f259ccd
Piotr Płaczek <99439005+piotrpdev@users.noreply.github.com>
2023-01-24 18:32:10 +0000

    better getname, nicer payslip, menu

--- src/main/kotlin/ie/setu/main.kt
+++ src/main/kotlin/ie/setu/main.kt
@@ -5,7 +5,7 @@ import java.text.DecimalFormat
 
 const val firstName = "Joe"
 const val surname = "Soap"
-const val gender = "m"
+const val gender = 'm'
 const val employeeId = 6143
 const val grossSalary = 67_543.21
 const val payePercentage = 0.385
@@ -13,7 +13,15 @@ const val prsiPercentage = 0.052
 const val annualBonusAmount = 1450.50
 const val cycleToWorkSchemeMonthlyDeduction = 54.33
 
-fun getFullName() = "${if (gender == "m") "MR." else "MS."} ${firstName.uppercase()} ${surname.uppercase()}"
+fun getFullName(): String {
+    val fullName = "$firstName $surname"
+
+    return when (gender) {
+        'm', 'M' -> "Mr. $fullName"
+        'f', 'F' -> "Ms. $fullName"
+        else -> fullName
+    }
+}
 
 fun getMonthlySalary() = grossSalary / 12
 fun getMonthlyBonus() = annualBonusAmount / 12
@@ -33,32 +41,64 @@ fun roundOff(number: Double): Double {
 fun getPayslip(): String {
     return """
             ------------------------------------------------------------------
-                Monthly Payslip                         
+                Monthly Payslip for ${getFullName()} (ID: $employeeId)                   
             __________________________________________________________________
-                    Employee ID: $employeeId                                                               
-                    Employee Name: ${getFullName()}                                                            
-            __________________________________________________________________                                           
-               PAYMENT DETAILS
+            
+            __________________________________________________________________ 
+               PAYMENT DETAILS (TOTAL: ${roundOff(getGrossMonthlyPay())})
             __________________________________________________________________
                     Salary: ${roundOff(getMonthlySalary())}
                     Bonus: ${roundOff(getMonthlyBonus())}
             __________________________________________________________________                                   
-               DEDUCTION DETAILS
+               DEDUCTION DETAILS (TOTAL: ${roundOff(getTotalMonthlyDeductions())})
             __________________________________________________________________
                     PAYE: ${roundOff(getMonthlyPAYE())}
                     PRSI: ${roundOff(getMonthlyPRSI())}
-                    Cycle To Work: $cycleToWorkSchemeMonthlyDeduction     
-            __________________________________________________________________
-                TOTALS
-            __________________________________________________________________ 
-                    Gross:  ${roundOff(getGrossMonthlyPay())}
-                    Total Deductions: ${roundOff(getTotalMonthlyDeductions())}
+                    Cycle To Work: $cycleToWorkSchemeMonthlyDeduction
+                    
             __________________________________________________________________
                     NET PAY: ${roundOff(getNetMonthlyPay())}                        
             __________________________________________________________________ 
         """.trimIndent()
 }
 
-fun main(args: Array<String>) {
-    println(getPayslip())
+fun menu() : Int {
+    print("""
+         Employee Menu for ${getFullName()}
+           1. Monthly Salary
+           2. Monthly PRSI
+           3. Monthly PAYE
+           4. Monthly Gross Pay
+           5. Monthly Total Deductions
+           6. Monthly Net Pay
+           7. Full Payslip
+          -1. Exit
+         Enter Option : """)
+    return readln().toInt()
+}
+
+
+fun main(args: Array<String>){
+    var input: Int
+
+    do {
+        input = menu()
+
+        println()
+
+        when(input) {
+            1 -> println("Monthly Salary: ${getMonthlySalary()}")
+            2 -> println("Monthly PRSI: ${getMonthlyPRSI()}")
+            3 ->println("Monthly PAYE: ${getMonthlyPAYE()}")
+            4 -> println("Monthly Gross Pay: ${getGrossMonthlyPay()}")
+            5 -> println("Monthly Total Deductions: ${getTotalMonthlyDeductions()}")
+            6 -> println("Monthly Net Pay: ${getNetMonthlyPay()}")
+            7 -> println(getPayslip())
+            -1 -> println("Exiting App")
+            else -> println("Invalid Option")
+        }
+
+        println()
+
+    } while (input != -1)
 }
